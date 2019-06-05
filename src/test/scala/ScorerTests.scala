eaea70a417978f2b08e0ebdd53f626649648190d
Benjamin Parker <benjamin@needstyping.com>
2019-06-05 11:33:47 +0100

    Changed Blue to Orange Allow single letter colour input

--- src/test/scala/ScorerTests.scala
+++ src/test/scala/ScorerTests.scala
@@ -6,26 +6,26 @@ class ScorerTests extends WordSpec with Matchers {
  "Mastermind Scorer" should {
 
    "score a perfect guess as 4 red and 0 white" in {
-     val makerRow = Row(Red, Red, White, Blue)
-     val breakerRow = Row(Red, Red, White, Blue)
+     val makerRow = Row(Red, Red, White, Orange)
+     val breakerRow = Row(Red, Red, White, Orange)
      Scorer.score(makerRow, breakerRow) shouldBe Score(4, 0)
    }
 
    "score a completely wrong guess as 0 red and 0 white" in {
      val makerRow = Row(Red, Red, Red, Red)
-     val breakerRow = Row(Blue, Blue, Blue, Blue)
+     val breakerRow = Row(Orange, Orange, Orange, Orange)
      Scorer.score(makerRow, breakerRow) shouldBe Score(0, 0)
    }
 
    "score a guess as 2 red and 2 white" in {
-     val makerRow = Row(Red, Red, White, Blue)
-     val breakerRow = Row(Red, Blue, White, Red)
+     val makerRow = Row(Red, Red, White, Orange)
+     val breakerRow = Row(Red, Orange, White, Red)
      Scorer.score(makerRow, breakerRow) shouldBe Score(2, 2)
    }
 
    "score a guess as 1 red and 1 white" in {
      val makerRow = Row(Red, Black, White, Yellow)
-     val breakerRow = Row(White, Black, Blue, Blue)
+     val breakerRow = Row(White, Black, Orange, Orange)
      Scorer.score(makerRow, breakerRow) shouldBe Score(1, 1)
    }
  }
--- src/test/scala/SolverTests.scala
+++ src/test/scala/SolverTests.scala
@@ -7,23 +7,23 @@ class SolverTests extends WordSpec with Matchers {
 
     "give 1 possibility for a score of 3 red" in {
       val perms = List(
-        Row(Red, White, Blue, Blue),
+        Row(Red, White, Orange, Orange),
         Row(Red, Red, Red, Red)
       )
-      val guess = Row(Red, White, Blue, Yellow)
-      Solver.filterPermutations(guess, Score(3, 0), perms) shouldBe List(Row(Red, White, Blue, Blue))
+      val guess = Row(Red, White, Orange, Yellow)
+      Solver.filterPermutations(guess, Score(3, 0), perms) shouldBe List(Row(Red, White, Orange, Orange))
     }
   }
 
   "Mastermind Solver with all permutations" should {
     "solve a game" in {
-      val maker = Row(Red, Green, Blue, White)
+      val maker = Row(Red, Green, Orange, White)
 
       def checkGuess(guess: Row) : Score = {
         Scorer.score(maker, guess)
       }
 
-      Solver.solve(checkGuess) shouldBe Row(Red, Green, Blue, White)
+      Solver.solve(checkGuess) shouldBe Row(Red, Green, Orange, White)
     }
   }
 }
